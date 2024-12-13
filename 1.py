e7325721912ef700bf680aa4fb1b15e34ab9d85a
Jkesava <jonnalagaddakesava80@gmail.com>
2024-12-13 21:04:25 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 17:36:46 2024
+@author: jonna
+"""
+"""n=int(input('enter n value'))
+sum=0
+for i in range(1,n+1):
+    if n%i==0:
+        sum=sum+i
+print(sum) """
+n=[2,3,4,5]       
+a=0
+for i in n:
+    a=a^i
+print(a)
+       
